84e98bf32c7663d9cfb5bedadcc58a2329c99970
DiegoAlcazar94 <diegoalcazarq@gmail.com>
2026-05-05 09:47:08 +0200

    Changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ProyectosComponent } from './pages/proyectos/proyectos.component';
+import { ContactoComponent } from './pages/contacto/contacto.component';
+import { ComunidadComponent } from './pages/comunidad/comunidad.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegistroComponent } from './pages/registro/registro.component';
+import { ProyectoDetalleComponent } from './pages/proyecto-detalle/proyecto-detalle.component';
+import { PagoComponent } from './pages/pago/pago.component';
+import { AboutComponent } from './pages/about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'proyectos', component: ProyectosComponent },
+  { path: 'proyectos/:id', component: ProyectoDetalleComponent },
+  { path: 'contacto', component: ContactoComponent },
+  { path: 'comunidad', component: ComunidadComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'registro', component: RegistroComponent },
+  { path: 'pago', component: PagoComponent },
+  { path: 'about', component: AboutComponent },
+  { path: '**', redirectTo: '' }
+];
