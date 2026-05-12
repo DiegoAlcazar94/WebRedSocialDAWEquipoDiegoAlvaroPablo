d5fbdf3ca4c3bfa8b526dff3bab6a6d6e6c6214a
ros1llo <alvaro.rosillo3@gmail.com>
2026-05-12 18:06:43 +0200

    feat: añadir método getProyectoById(id) al ProyectoService

--- src/app/services/proyecto.service.ts
+++ src/app/services/proyecto.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { Proyecto } from '../interfaces/proyecto';
 
 @Injectable({
@@ -14,5 +15,11 @@ export class ProyectoService {
 
   getProyectos(): Observable<Proyecto[]> {
     return this.http.get<Proyecto[]>(this.dataUrl);
+  }
+
+  getProyectoById(id: number): Observable<Proyecto | undefined> {
+    return this.http.get<Proyecto[]>(this.dataUrl).pipe(
+      map(proyectos => proyectos.find(p => p.id === id))
+    );
   }
 }
